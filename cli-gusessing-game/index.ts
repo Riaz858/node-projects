4ea59dd0784c50c5616ee98508f03d1aaaddad61
Riaz Ahmed <riaz.ahmed.nr262@gmail.com>
2024-03-26 02:56:25 +0500

    cli guessing game

---
+++ cli-gusessing-game/index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 5 + 1)
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number",
+  },
+]);
+
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congratulation you guessed a right number")
+}
+else{
+    console.log("Sorry you guessed wrong number")
+}
